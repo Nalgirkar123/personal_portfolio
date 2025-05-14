69dde465dc314d508659b3e62f3f819a515a321e
omkar-dev <omkarnalgirkar17@gmail.com>
2025-05-14 18:30:49 +0530

    logging change

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -115,27 +115,17 @@ AUTH_PASSWORD_VALIDATORS = [
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '{levelname} {asctime} {module} {message}',
-            'style': '{',
-        },
-    },
     'handlers': {
-        'console': {
+        'console': {  # Change to console handler for serverless
+            'level': 'DEBUG',
             'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
         },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'INFO',
     },
     'loggers': {
         'django': {
             'handlers': ['console'],
-            'level': 'INFO',
-            'propagate': False,
+            'level': 'DEBUG',
+            'propagate': True,
         },
     },
 }
